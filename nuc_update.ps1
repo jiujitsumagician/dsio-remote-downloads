d46af0febfc88f86dedae066933c15c877b883e5
jiujitsumagician <150487413+jiujitsumagician@users.noreply.github.com>
2026-09-25 12:38:22 -0400

    remove one-off nuc_update.ps1

--- nuc_update.ps1
+++
@@ -1,4 +0,0 @@
-$dst="$env:TEMP\DsioRemoteAgentSetup.exe"
-[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://github.com/jiujitsumagician/dsio-remote-downloads/releases/download/v2.1.0/DsioRemoteAgentSetup.exe" -OutFile $dst -UseBasicParsing
-Start-Process $dst -ArgumentList '/update'
